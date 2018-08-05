482c2beb274d792fc6f868c8375dab49b4bcb7a1
Dmitrij Rusakov <daemon2017@gmail.com>
2018-08-05 17:27:07 +0300

    Выходные значения согласованы со значениями FRD

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import csv
 
 from numpy import genfromtxt
 from mymodel import build, matrix_width, matrix_height
@@ -29,6 +30,12 @@ def func_predict():
     i = 0
     for prediction in predictions:
         prediction = np.argmax(prediction, 2)
+        shape = prediction.shape
+        for r in range(0, shape[0]):
+            for c in range(0, shape[1]):
+                for compliance_row in compliance:
+                    if compliance_row[1] == prediction[r, c]:
+                        prediction[r, c] = compliance_row[0]
         short_name = os.path.splitext(x_files_names[i])[0]
         np.savetxt('./predict_output/' + str(short_name) + '.csv', prediction, fmt="%s", delimiter=',')
         i += 1
@@ -38,6 +45,12 @@ if not os.path.exists('predict_input'):
     os.makedirs('predict_input')
 if not os.path.exists('predict_output'):
     os.makedirs('predict_output')
+
+compliance = []
+with open("compliance.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    for row in reader:
+        compliance.append(row)
 
 model = build()
 model.load_weights('weights_batch.h5')
