fabdee3423b410b29497a454188e5b0224d3d1bf
Dmitrij Rusakov <daemon2017@gmail.com>
2019-04-06 22:15:32 +0300

    Функция потерь заменена на F1

--- mymodel.py
+++ mymodel.py
@@ -1,4 +1,5 @@
 from keras import backend as K
+import tensorflow as tf
 from keras.layers import Conv2D, Input, Conv2DTranspose, BatchNormalization, add, \
     SpatialDropout2D, Activation
 from keras.losses import categorical_crossentropy
@@ -17,21 +18,32 @@ epochs_num = 100
 
 
 def f1(y_true, y_pred):
-    def recall(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + K.epsilon())
-        return recall
+    y_pred = K.round(y_pred)
+    tp = K.sum(K.cast(y_true * y_pred, 'float'), axis=0)
+    # tn = K.sum(K.cast((1 - y_true) * (1 - y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1 - y_true) * y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true * (1 - y_pred), 'float'), axis=0)
 
-    def precision(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + K.epsilon())
-        return precision
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
 
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
-    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+    f1 = 2 * p * r / (p + r + K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return K.mean(f1)
+
+
+def f1_loss(y_true, y_pred):
+    tp = K.sum(K.cast(y_true * y_pred, 'float'), axis=0)
+    # tn = K.sum(K.cast((1 - y_true) * (1 - y_pred), 'float'), axis=0)
+    fp = K.sum(K.cast((1 - y_true) * y_pred, 'float'), axis=0)
+    fn = K.sum(K.cast(y_true * (1 - y_pred), 'float'), axis=0)
+
+    p = tp / (tp + fp + K.epsilon())
+    r = tp / (tp + fn + K.epsilon())
+
+    f1 = 2 * p * r / (p + r + K.epsilon())
+    f1 = tf.where(tf.is_nan(f1), tf.zeros_like(f1), f1)
+    return 1 - K.mean(f1)
 
 
 def dice_coef(y_true, y_pred):
@@ -110,7 +122,7 @@ def build():
     model = Model(inputs=[inputs],
                   outputs=[output])
     model.compile(optimizer=Adam(lr=L_0),
-                  loss=categorical_crossentropy,
+                  loss=f1_loss,
                   metrics=[dice_coef, f1])
     print('Model is ready!')
     print(model.summary())
